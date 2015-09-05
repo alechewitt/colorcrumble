93f4eb35081c279faf09e1656789ebed93a22f26
Alec Hewitt <alec.hewitt9@gmail.com>
2015-09-05 19:50:05 +0100

    Addition of new textures

--- src/counters.js
+++ src/counters.js
@@ -1,32 +1,102 @@
 import * as colors from "./color-options.js";
 export default [
+    // Red Group
     {
-        textureUrl: "/textures/twoThirdsTest.png",
+        textureUrl: "/textures/half8.png",
         colorGroup: colors.RED,
         description: "Two thirds"
     },
     {
-        textureUrl: "/textures/testImage2.png",
+        textureUrl: "/textures/2Quarters.png",
+        colorGroup: colors.RED,
+        description: "Two thirds"
+    },
+    {
+        textureUrl: "/textures/0Point5.png",
+        colorGroup: colors.RED,
+        description: "Two thirds"
+    },
+
+    // Blue Group
+    {
+        textureUrl: "/textures/twoSquared.png",
         colorGroup: colors.BLUE,
         description: "Two thirds"
     },
     {
-        textureUrl: "/textures/twoThirdsTest.png",
+        textureUrl: "/textures/four.png",
+        colorGroup: colors.BLUE,
+        description: "Two thirds"
+    },
+    {
+        textureUrl: "/textures/eightHalves.png",
+        colorGroup: colors.BLUE,
+        description: "Two thirds"
+    },
+
+    // Orange Group
+    {
+        textureUrl: "/textures/oneThird.png",
+        colorGroup: colors.ORANGE,
+        description: "Two thirds"
+    },{
+        textureUrl: "/textures/twoSixths.png",
         colorGroup: colors.ORANGE,
         description: "Two thirds"
     },
     {
-        textureUrl: "/textures/testImage2.png",
+        textureUrl: "/textures/threeNinths.png",
+        colorGroup: colors.ORANGE,
+        description: "Two thirds"
+    },
+
+    // Yellow Group
+    {
+        textureUrl: "/textures/0Point25.png",
         colorGroup: colors.YELLOW,
         description: "Two thirds"
     },
     {
-        textureUrl: "/textures/twoThirdsTest.png",
+        textureUrl: "/textures/oneQuarter.png",
+        colorGroup: colors.YELLOW,
+        description: "Two thirds"
+    },
+    {
+        textureUrl: "/textures/2Eighths.png",
+        colorGroup: colors.YELLOW,
+        description: "Two thirds"
+    },
+
+    // Green Group
+    {
+        textureUrl: "/textures/threeSquared.png",
         colorGroup: colors.GREEN,
         description: "Two thirds"
     },
     {
-        textureUrl: "/textures/twoThirdsTest.png",
+        textureUrl: "/textures/nine.png",
+        colorGroup: colors.GREEN,
+        description: "Two thirds"
+    },
+    {
+        textureUrl: "/textures/threeSquared.png",
+        colorGroup: colors.GREEN,
+        description: "Two thirds"
+    },
+
+    // Purple Group
+    {
+        textureUrl: "/textures/fourSquared.png",
+        colorGroup: colors.PURPLE,
+        description: "Two thirds"
+    },
+    {
+        textureUrl: "/textures/fourSquared.png",
+        colorGroup: colors.PURPLE,
+        description: "Two thirds"
+    },
+    {
+        textureUrl: "/textures/sixteen.png",
         colorGroup: colors.PURPLE,
         description: "Two thirds"
     }
