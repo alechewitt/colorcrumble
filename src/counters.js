5dcd8b1396f1a666a4141483163d040f1668e2a5
Alec Hewitt <alec.hewitt9@gmail.com>
2015-09-09 16:15:05 +0100

    Update counters with correct description

--- src/counters.js
+++ src/counters.js
@@ -4,101 +4,101 @@ export default [
     {
         textureUrl: "/textures/half8.png",
         colorGroup: colors.RED,
-        description: "Two thirds"
+        description: "Half as a fraction"
     },
     {
         textureUrl: "/textures/2Quarters.png",
         colorGroup: colors.RED,
-        description: "Two thirds"
+        description: "Two Quarters as a fraction"
     },
     {
         textureUrl: "/textures/0Point5.png",
         colorGroup: colors.RED,
-        description: "Two thirds"
+        description: "Half as decimal representation"
     },
 
     // Blue Group
     {
         textureUrl: "/textures/twoSquared.png",
         colorGroup: colors.BLUE,
-        description: "Two thirds"
+        description: "Two Squared"
     },
     {
         textureUrl: "/textures/four.png",
         colorGroup: colors.BLUE,
-        description: "Two thirds"
+        description: "Four"
     },
     {
         textureUrl: "/textures/eightHalves.png",
         colorGroup: colors.BLUE,
-        description: "Two thirds"
+        description: "Eight Halves"
     },
 
     // Orange Group
     {
         textureUrl: "/textures/oneThird.png",
         colorGroup: colors.ORANGE,
-        description: "Two thirds"
+        description: "One third"
     },{
         textureUrl: "/textures/twoSixths.png",
         colorGroup: colors.ORANGE,
-        description: "Two thirds"
+        description: "Two Sixths"
     },
     {
         textureUrl: "/textures/threeNinths.png",
         colorGroup: colors.ORANGE,
-        description: "Two thirds"
+        description: "Three ninths"
     },
 
     // Yellow Group
     {
         textureUrl: "/textures/0Point25.png",
         colorGroup: colors.YELLOW,
-        description: "Two thirds"
+        description: "One quarter as decimal representation"
     },
     {
         textureUrl: "/textures/oneQuarter.png",
         colorGroup: colors.YELLOW,
-        description: "Two thirds"
+        description: "One quarter as fraction"
     },
     {
         textureUrl: "/textures/2Eighths.png",
         colorGroup: colors.YELLOW,
-        description: "Two thirds"
+        description: "Two eighths"
     },
 
     // Green Group
     {
         textureUrl: "/textures/threeSquared.png",
         colorGroup: colors.GREEN,
-        description: "Two thirds"
+        description: "Three squared"
     },
     {
         textureUrl: "/textures/nine.png",
         colorGroup: colors.GREEN,
-        description: "Two thirds"
+        description: "Nine"
     },
     {
         textureUrl: "/textures/threeSquared.png",
         colorGroup: colors.GREEN,
-        description: "Two thirds"
+        description: "Three squared"
     },
 
     // Purple Group
     {
         textureUrl: "/textures/fourSquared.png",
         colorGroup: colors.PURPLE,
-        description: "Two thirds"
+        description: "Four squared"
     },
     {
         textureUrl: "/textures/fourSquared.png",
         colorGroup: colors.PURPLE,
-        description: "Two thirds"
+        description: "Four squared"
     },
 
     {
         textureUrl: "/textures/sixteen.png",
         colorGroup: colors.PURPLE,
-        description: "Two thirds"
+        description: "Sixteen"
     }
 ]
